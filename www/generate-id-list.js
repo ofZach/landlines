d32b0748e45f5beba10cb3584c2536334b237625
Matt Felsen <mattfelsen@gmail.com>
2016-11-04 18:23:12 -0400

    Draw: update to read metadata re: #12

--- www/generate-id-list.js
+++ www/generate-id-list.js
@@ -4,6 +4,7 @@ var ids = [];
 var dir = fs.readdirSync('draw/metadata');
 for (var i = 0; i < dir.length; i++) {
 	var filename = dir[i];
+	if (filename.substr(0, 1) == '.') continue;
 	ids.push(filename.substr(0, filename.length - 5))
 }
 
